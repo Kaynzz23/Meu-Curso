7c5111bf439f6a9987242413de241671e82805da
Kaynzz23 <112899969+Kaynzz23@users.noreply.github.com>
2024-05-15 23:23:43 -0300

    Meu Curso

---
+++ Curso/Linguagens/Html/exercicios/Meu Exercicio/calculadora/script.js
@@ -0,0 +1,55 @@
+let one = document.getElementsByClassName('one');
+let two = document.getElementsByClassName('two');
+let three = document.getElementsByClassName('three');
+let four = document.getElementsByClassName('four');
+let five = document.getElementsByClassName('five');
+let six = document.getElementsByClassName('six');
+let seven = document.getElementsByClassName('seven');
+let eight = document.getElementsByClassName('eight');
+let nine = document.getElementsByClassName('nine');
+let zero = document.getElementsByClassName('zero');
+let zerozero = document.getElementsByClassName('zerozero');
+
+one.addEventListener('click', () => {
+    console.log('Button 1 was clicked');
+});
+
+two.addEventListener('click', () => {
+    console.log('Button 2 was clicked');
+});
+
+three.addEventListener('click', () => {
+    console.log('Button 3 was clicked');
+});
+
+four.addEventListener('click', () => {
+    console.log('Button 4 was clicked');
+});
+
+five.addEventListener('click', () => {
+    console.log('Button 5 was clicked');
+});
+
+six.addEventListener('click', () => {
+    console.log('Button 6 was clicked');
+});
+
+seven.addEventListener('click', () => {
+    console.log('Button 7 was clicked');
+});
+
+eight.addEventListener('click', () => {
+    console.log('Button 8 was clicked');
+});
+
+nine.addEventListener('click', () => {
+    console.log('Button 9 was clicked');
+});
+
+zero.addEventListener('click', () => {
+    console.log('Button 0 was clicked');
+});
+
+zerozero.addEventListener('click', () => {
+    console.log('Button 00 was clicked');
+});
